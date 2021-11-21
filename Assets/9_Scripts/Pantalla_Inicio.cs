bb81fe185f952f0999308d473eb56723cd4e05be
AndreaGrcia30 <angarcia@cedim.edu.mx>
2021-11-21 17:45:23 -0600

    Boton Load
    Configure el boton load pero no se si funciona xd

--- Assets/9_Scripts/Pantalla_Inicio.cs
+++ Assets/9_Scripts/Pantalla_Inicio.cs
@@ -2,13 +2,29 @@
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+using System.IO;
+using GameLib.MemorySystem;
 
 public class Pantalla_Inicio : MonoBehaviour
 {
+     [SerializeField]
+    Button btnLoadGame;
     // Start is called before the first frame update
     void Start()
     {
         Time.timeScale = 1;
+    }
+
+    void Awake() {
+        {
+             btnLoadGame.onClick.AddListener(LoadGame);
+        }
+    }
+
+      void LoadGame()
+    {
+        MemorySystem.LoadGame("gamedata");
     }
 
     // Update is called once per frame
